b1c87bd4efafdbf5a28f215f09010aec64815957
final4800 <shilinzheng@slu.edu>
2018-12-04 23:24:32 -0600

    1

---
+++ 4800shilin.R
@@ -0,0 +1,10 @@
+#' 4800shilin package
+#' 
+#' R package that implements sampling from a continuous random variable.
+#' 
+#' @docType package
+#'
+#' @author shilin zheng \email{shilinzheng@slu.edu}
+#' 
+#' @name 4800shilin
+NULL
