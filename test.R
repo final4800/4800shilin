78196a02803985d929c337430d657dfceb1a54c5
final4800 <shilinzheng@slu.edu>
2018-12-04 21:21:47 -0600

    5

--- test.R
+++ test.R
@@ -1,3 +1,10 @@
 square <- function(x){
   return(x^2)
 }
+# Install devtools from CRAN
+install.packages("devtools")
+
+# Or the development version from GitHub:
+# install.packages("devtools")
+devtools::install_github("r-lib/devtools")
+
