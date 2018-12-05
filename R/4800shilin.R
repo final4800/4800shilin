7b7ec18d8733602318a318292607085b224aa1a3
final4800 <shilinzheng@slu.edu>
2018-12-05 14:42:41 -0600

    125

---
+++ R/4800shilin.R
@@ -0,0 +1,10 @@
+#' package4800
+#' 
+#' sampling from continuous distributions, given their pdfs
+#' 
+#' @docType package 
+#'
+#' @author shilin zheng \email{shilinzheng@slu.edu}
+#' 
+#' @name 4800shilin
+NULL
