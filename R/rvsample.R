842c7781182f0ee90a155f339f42f833111652c6
final4800 <shilinzheng@slu.edu>
2018-12-06 15:33:40 -0600

    pdf126

--- R/rvsample.R
+++ R/rvsample.R
@@ -1,18 +1,21 @@
+#' 4800shilin
+#' 
 #' what does the function do: implements sampling from a continuous random variable.
 #'
-#' what does the function do: implements sampling from a continuous random variable.
-
-ImportS: ggplot2
-
+#' @description what does the function do: implements sampling from a continuous random variable.
 #'
 #' @param n the number of samples
-#' @param f a function that is the pdf of the random variable that you wish to sample from
-#' @param a a numeric that is the lower bound of the random variable you wish to sample from
-#' @param b a numeric that is the upper bound of the random variable you wish to sample from m, so P(a ≤ X ≤ b) = 1.
+#' @param pdf a function that is the pdf of the random variable that you wish to sample from
+#' @param lb a numeric that is the lower bound of the random variable you wish to sample from
+#' @param ub a numeric that is the upper bound of the random variable you wish to sample from m, so P(a ≤ X ≤ b) = 1.
 #' @param c a numeric that is such that f(x) ≤ C for all values of x
  
 #' @return a random sample of size n from the rv with pdf provided to the function, using rejection sampling
 #' @export
 #' 
 
-rvsample <- function(n, f, a, b, c)  
+rvsample <- function(n, pdf, lb, ub, c){
+  integratepdf <- integrate(pdf,lb,ub)
+  if(!(intergratepdf[1] == 1)){stop("this is not a pdf P(a ≤ X ≤ b) = 1")}
+  if(ub < lb){stop("lb lower bound has to be less than the ub upper bound")}
+  if(n <= 0){stop("n should be a positive integer number")}}
