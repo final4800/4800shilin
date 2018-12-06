a302641906d885e4934549c00d9c0afc3be9c10a
final4800 <shilinzheng@slu.edu>
2018-12-06 17:21:10 -0600

    q

--- R/rvsample.R
+++ R/rvsample.R
@@ -7,8 +7,8 @@
 #' @param n the number of samples
 #' @param pdf a function that is the pdf of the random variable that you wish to sample from
 #' @param lb a numeric that is the lower bound of the random variable you wish to sample from
-#' @param ub a numeric that is the upper bound of the random variable you wish to sample from m, so P(a ≤ X ≤ b) = 1.
-#' @param c a numeric that is such that f(x) ≤ C for all values of x
+#' @param ub a numeric that is the upper bound of the random variable you wish to sample from m, so P(a <= X <= b) = 1.
+#' @param c a numeric that is such that f(x) <= C for all values of x
  
 #' @return a random sample of size n from the rv with pdf provided to the function, using rejection sampling
 #' @export
