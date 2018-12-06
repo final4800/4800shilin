081c0ac3969828b91bc8ae2298693ab2fac0abd3
final4800 <shilinzheng@slu.edu>
2018-12-05 23:47:42 -0600

    1

---
+++ R/rvsample.R
@@ -0,0 +1,18 @@
+#' what does the function do: implements sampling from a continuous random variable.
+#'
+#' what does the function do: implements sampling from a continuous random variable.
+
+ImportS: ggplot2
+
+#'
+#' @param n the number of samples
+#' @param f a function that is the pdf of the random variable that you wish to sample from
+#' @param a a numeric that is the lower bound of the random variable you wish to sample from
+#' @param b a numeric that is the upper bound of the random variable you wish to sample from m, so P(a ≤ X ≤ b) = 1.
+#' @param c a numeric that is such that f(x) ≤ C for all values of x
+ 
+#' @return a random sample of size n from the rv with pdf provided to the function, using rejection sampling
+#' @export
+#' 
+
+rvsample <- function(n, f, a, b, c)
